d02dc01ad6dfb6c274143ad22dc5e84aa23963ee
xodud001 <freeptk1@gmail.com>
2021-04-10 13:53:00 +0900

    컴포넌트 만들기Class로 컴포넌트 만들기, Hook으로 만들기

--- src/index.js
+++ src/index.js
@@ -2,7 +2,38 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+function HookMessage(props){
+    return(
+        <li className="welcome hook">Hello, This is Hook!</li>    
+    );
+}
+
+class ClassMessage extends Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <li className="welcome class">Hello, This is Class!</li>    
+        );
+    }
+}
+
+const Main = (props) =>{
+    return(
+        <>
+            <div className="welcome class">Hello, This is Main</div>
+            <ul>
+                <HookMessage/>
+                <ClassMessage/>
+            </ul>
+        </>
+    );
+}
+
 ReactDOM.render(
-    <div className="welcome">Hello, React!</div>,
+    <Main/>,
     document.getElementById('root')
 );
