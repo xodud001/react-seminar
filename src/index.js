0b0d7b75040e12fd710d5b15f22eca68b576d03e
xodud001 <freeptk1@gmail.com>
2021-04-11 11:36:32 +0900

    컴포넌트 만드는 방법에 주석 달기

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+
+//Hook을 이용한 컴포넌트 만들기
 function HookMessage(props){
     return(
         <li className="welcome hook">Hello, This is Hook!</li>    
     );
 }
 
+// 화살표 함수를 이용한 컴포넌트 만들기
+const Main = (props) =>{
+    return(
+        <>
+            <div className="welcome class">Hello, This is Main</div>
+            <ul>
+                <HookMessage/>
+                <ClassMessage/>
+            </ul>
+        </>
+    );
+}
+
+//클래스를 이용한 컴포넌트 만들기
 class ClassMessage extends Component{
 
     constructor(props){
@@ -21,17 +37,7 @@ class ClassMessage extends Component{
     }
 }
 
-const Main = (props) =>{
-    return(
-        <>
-            <div className="welcome class">Hello, This is Main</div>
-            <ul>
-                <HookMessage/>
-                <ClassMessage/>
-            </ul>
-        </>
-    );
-}
+
 
 ReactDOM.render(
     <Main/>,
